38239cb4f49fc499df20e4dbac3b80f54cf21534
bauer96 <zocken210@web.de>
2022-04-30 04:04:15 +0200

    update7

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.className === "nav-bar") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav-bar";
-    }
-  }
 
 const icon = document.getElementById('moon-icon');
 
