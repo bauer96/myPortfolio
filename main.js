97b1b89e880674ef2dab8c6c69052fa50c502ed6
bauer96 <zocken210@web.de>
2022-04-30 03:53:31 +0200

    update6

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-const toogleButton = document.getElementById('toogle-button');
-const navList = document.getElementById('navList');
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "nav-bar") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav-bar";
+    }
+  }
 
-toogleButton.addEventListener('click', () => {
-    navList.classList.toggle('active');
-});
+const icon = document.getElementById('moon-icon');
+
+icon.onclick = function() {
+    document.body.classList.toggle('light-theme');
+    if (document.body.classList.contains('light-theme')) {
+        icon.src = "ressoures/img/moon.png";
+        
+    } else {
+        icon.src = "ressoures/img/sun.png";
+       
+    }
+}
